d2c4e6ec8d0de27d2121ace0713c154f3bac57f9
AmeliaMarifah <marifahamelia8@gmail.com>
2026-05-29 21:42:09 +0700

    add animateCounter function for animated number display

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function animateCounter(id, target, suffix = '') {
+  let start = 0;
+
+  const duration = 2000;
+  const step = target / (duration / 16);
+  const timer = setInterval(() => {
+    start = Math.min(start + step, target);
+
+    $('#' + id).text(
+      Math.floor(start).toLocaleString('id-ID') + suffix
+    );
+
+    if (start >= target) {
+      clearInterval(timer);
+    }
+  }, 16);
+}
+
 function toggleMenu() {
   $('#nav-links').toggleClass('open');
 }
